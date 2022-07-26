bdd90d9ff77589fe578dad0e773d517a4b37c5bf
bundi-xris <bundichris1400@gmail.com>
2022-07-27 01:41:35 +0300

    use .env Jwtscret on the test

--- test_main.py
+++ test_main.py
@@ -8,11 +8,16 @@ import pytest
 
 import main
 load_dotenv()
-SECRET = os.getenv('SECRET')
-TOKEN = os.getenv('TOKEN')
-EMAIL = os.getenv('EMAIL')
-PASSWORD =os.getenv('PASSWORD')
+# SECRET = os.getenv('SECRET')
+# TOKEN = os.getenv('TOKEN')
+# EMAIL = os.getenv('EMAIL')
+# PASSWORD =os.getenv('PASSWORD')
 
+SECRET = os.getenv('JWT_SECRET')
+
+TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
+EMAIL = 'wolf@thedoor.com'
+PASSWORD = 'huff-puff'
 
 @pytest.fixture
 def client():
